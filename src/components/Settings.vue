49e379cd3f9372d3acf096f9059aade3ce4ace78
Дмитрий Мартынов <dmitrijmartynov@MacBook-Air-Dmitrij.local>
2024-08-22 20:16:27 +0300

    update

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -46,28 +46,6 @@
 
 
 
-{{ inputMax }}
-
-
-<!-- <div>
-                <p class="block mb-2 font-sans text-sm antialiased font-medium leading-normal text-gray-900">
-                  Максимальное количество калорий
-                </p>
-                <div class="relative h-10 w-full min-w-[200px]">
-                  <input placeholder="123" v-model="inputMax"
-                    class="peer h-full w-full rounded-[7px] border border-gray-200  px-3 py-2.5 font-sans text-sm font-normal text-gray-700 outline outline-0" />
-
-                </div>
-              </div>
-
-              <button @click="editMax()"
-                class="select-none rounded-lg bg-gray-900 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-2"
-                type="button">
-                Изменить
-              </button> -->
-
-
-
 
               <div>
                 <p class="block mb-2 font-sans text-sm antialiased font-medium leading-normal text-gray-900">
