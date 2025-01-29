0b69746d173c0c9442bcbfcd77a5c6122d471bbd
Дмитрий Мартынов <dmitrijmartynov@MacBook-Air-Dmitrij.local>
2025-01-29 12:26:50 +0300

    backend_fix

--- back_new.js
+++ back_new.js
@@ -8,7 +8,7 @@ const middlewares = defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = 4000; // Change the port if needed
+const PORT = 6000; // Change the port if needed
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
