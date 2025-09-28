1d2280c0a371c58c83074e867918e7268271a527
mrtynnvv <dm346@mail.ru>
2025-09-28 19:14:26 +0300

    upd

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,9 @@ import Feed from '@/pages/Feed/index.vue'
 import Filing from '@/pages/Filing/index.vue'
 import Profile from '@/pages/Profile/index.vue'
 import Scales from '@/pages/Scales/index.vue'
-import timer from '@/pages/secret/timer.vue'
-import game from '@/pages/secret/game.vue'
+import timer from '@/secret/timer.vue'
+import game from '@/secret/game.vue'
 import Settings from '@/pages/Settings/index.vue'
-
 const routes = [
   { path: '/', redirect: '/feed' },
   { path: '/feed', component: Feed },
